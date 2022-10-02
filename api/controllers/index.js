a8921eb9ceb7840cf2cfac58407a743edc042841
blue-bigtech <blue.bigtech@gmail.com>
2022-10-02 11:30:26 -0400

    web3 but some issue

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,36 +1,69 @@
-const fs = require('fs');
+const fs = require('fs')
+const Web3 = require('web3')
 const { matchedData } = require('express-validator')
-const { pos } = require('../../config')
-const { getPOSClient, to } = require('../utils/POSClient')
+const { raw } = require('body-parser')
+const Common = require('ethereumjs-common')
+const web3 = new Web3(new Web3.providers.HttpProvider('https://rpc-mumbai.maticvigil.com/'))
 
-const transferERC20 = async () => {
-  const client = await getPOSClient();
-  const erc20Token = client.erc20(pos.parent.erc20, true);
-
-  const result = await erc20Token.transfer(100, to, {
-      gasPrice: '30000000000',
-  });
-
-  const txHash = await result.getTransactionHash();
-  console.log("txHash", txHash);
-  const receipt = await result.getReceipt();
-  console.log("receipt", receipt);
-
-}
 
 const transfer = async (req, res) => {
   const data = matchedData(req)
-  // const rootPath = __dirname.substring(0, __dirname.indexOf('api'));
-  // const abi = fs.readFileSync('abi/AKYT.json','utf-8')
-
-  transferERC20().then(() => {
-  }).catch(err => {
-      console.error("err", err);
-  }).finally(_ => {
-      process.exit(0);
-  })
+  const rootPath = __dirname.substring(0, __dirname.indexOf('api'));
+  const abi = fs.readFileSync('abi/AKYT.json','utf-8')
   
-  res.status(200).json({ transfer: 'ok' })
+  const myAddress = "0xC4005Bc827dd003629C95D9Ca62873b8a053d2F9"
+  const toAddress = "0x321D181c7e4D777C577DCBf78db75bB70a227df4"
+  const contractAddress = "0x803c7E3e7db4CD812D48623CAFFC6bd84bE16E8D"
+  const amount = web3.utils.toHex(100);
+  
+  let count = await web3.eth.getTransactionCount(myAddress)
+  console.log(count)
+  var privateKey = new Buffer.from('68ddf6cc1cccaffaffc46c6899222a19a95a57f5232a938ed55c0ebce41230af', 'hex');
+  const contract = new web3.eth.Contract(JSON.parse(abi), contractAddress, {from: myAddress})
+  let rawData = contract.methods.transfer(toAddress, amount).encodeABI();
+  let gasPrice = web3.eth.gasPrice;
+  const rawTransaction = {
+    "from":myAddress, 
+    "gasPrice":web3.utils.toHex(gasPrice),
+    "gasLimit":web3.utils.toHex(210000),
+    "to":contractAddress,
+    "value":"0x0",
+    "data": rawData,
+    "nonce":web3.utils.toHex(count)
+  }
+  var Tx = require('@ethereumjs/tx').Transaction;
+  // const commonMatic = Common.default.forCustomChain(
+  //   'matic',
+  //   {
+  //     name: 'matic-testnet',
+  //     networkId: 80001,
+  //     chainId: 80001,
+  //     url: 'https://rpc-mumbai.maticvigil.com/'
+  //   },
+  //   'petersburg'
+  // )
+
+  const common = Common.default.forCustomChain(
+    'ropsten', 
+    {
+      name: 'eth',
+      networkId: 3,
+      chainId: 3
+    },
+  'petersburg'
+  );
+  
+  var tx = new Tx(rawTransaction, { common: common },);
+  const signedTx = tx.sign(privateKey)
+  const serializedTx = signedTx.serialize();
+  // const transaction = new Transaction(rawTransaction)
+  // transaction.sign(privateKey)
+  web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+    .on('receipt', console.log)
+
+  const balance = await contract.methods.balanceOf(myAddress).call();
+
+  res.status(200).json({ transfer: 'ok', balance })
 }
 
 const mint = async (req, res) => {
